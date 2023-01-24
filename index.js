a8e473313964ca750ed631ca7f1f7a6a800bfe3c
Johannlinzen <jliebem@gmail.com>
2023-01-24 14:53:08 +0100

    fix db

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require("dotenv/config");
+
 const express = require("express");
 const { userRouter } = require("./routes/users");
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(express.json());
 
